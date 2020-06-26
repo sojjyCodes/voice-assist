203589442eaf9abbc5325326cfe37cc942d484e9
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-06-26 19:36:23 +0100

    First commit

--- Voice assistant.py
+++ Voice assistant.py
@@ -1,42 +1,47 @@
+import pyttsx3
+engine = pyttsx3.init()
+
 operation = input('''
 Please choose 1 of the following
-1 for About Me
+1 for About assistant
 2 for About Owner 
-3 for owners github
+3 for my GitHub username
 4 for what programming language does he use
 5 for exit
 ''')
-import pyttsx3
-
-def oper(About):
-    if operation == '1':
-        pyttsx3.speak("My, name is NZ, i was, created by my, lovely master, sojjyCodes")
-        print("My, name is NZ, i was, created by my, lovely master, sojjyCodes")
-oper("about")
-
-def oper(Owner):
-    if operation == '2':
-        pyttsx3.speak("The name, of my creator is Ebenezer, he is 13 years old.")
-        print("The name, of my creator is Ebenezer, he is 13 years old.")
-oper("Owner")
-
-def oper(hub):
-    if operation == '3':
-        pyttsx3.speak("sojjy's github is sojjyCodes")
-        print("https://www.github.com/sojjyCodes/")
-oper("hub")
-
-def oper(lang):
-    if operation == '4':
-        pyttsx3.speak("His main programming language is python ")
-    print("His main programming language is python ")
-oper("lang")
-
-if operation == '5':
-    pyttsx3.speak("Thanks for visiting ")
-    print("Thanks for visiting ")
-    exit()
-oper("Thanks")
 
 
+def about_assistant():
+    pyttsx3.speak("My name, is NZ. And i was created by my lovely master sojjyCodes")
+    print("My name, is, NZ. And i, was created by, my master, sojjyCodes.")
 
+
+def about_master():
+    pyttsx3.speak("The name, of my creator, is, sojjyCodes ")
+    print("The name of my creator is, sojjyCodes ")
+
+
+def github_username():
+    pyttsx3.speak("The username, is, sojjyCodes")
+    print("https://github.com/sojjyCodes")
+
+
+def my_programming_language():
+    pyttsx3.speak("My master, uses the python, programming language. ")
+    print("Python")
+
+
+def process_operation_gotten_from_user(operation_from_user):
+    if operation_from_user == '1':
+        about_assistant()
+    if operation_from_user == '2':
+        about_master()
+    if operation_from_user == '3':
+        github_username()
+    if operation_from_user == '4':
+        my_programming_language()
+    else:
+        exit(1)
+
+
+process_operation_gotten_from_user(operation)
