d7d2555b7c8046426d506faf50279c8ea4fcb451
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-19 21:30:14 +0100

    Trying to fix a bug

--- Voice assistant.py
+++ Voice assistant.py
@@ -7,8 +7,15 @@ Please choose 1 of the following
 2 for About Owner 
 3 for my GitHub username
 4 for what programming language does he use
-5 for exit
+5 for open Chrome 
+6 for open Firefox
+7 for exit
+8 to fully shutdown system
 ''')
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver2 = webdriver.Firefox()
 
 
 def about_assistant():
@@ -22,13 +29,35 @@ def about_master():
 
 
 def github_username():
-    pyttsx3.speak("The username, is, sojjyCodes")
+    pyttsx3.speak("My master github is sojjyCodes")
     print("https://github.com/sojjyCodes")
 
 
 def my_programming_language():
     pyttsx3.speak("My master, uses the python, programming language. ")
     print("Python")
+
+
+def openChrome():
+    driver.get('https://github.com/sojjyCodes')
+    pyttsx3.speak('Opening, Chrome')
+    print('Opening Chrome')
+
+def openFirefox():
+    driver2.get('https://github.com/sojjyCodes')
+    pyttsx3.speak('Opening, Firefox')
+    print('Opening Firefox')
+
+
+def exit():
+    pyttsx3.speak('Exiting, Program')
+    print("Exiting Program")
+    exit()
+
+
+def remotely_shutdown_system():
+    pyttsx3.speak("Shutting, down")
+    os.system("poweroff")
 
 
 def process_operation_gotten_from_user(operation_from_user):
@@ -40,8 +69,14 @@ def process_operation_gotten_from_user(operation_from_user):
         github_username()
     if operation_from_user == '4':
         my_programming_language()
-    else:
-        exit(1)
+    if operation_from_user == '5':
+        openChrome()
+    if operaton_from_user == '6':
+        openFirefox()
+    if operation_from_user == '7':
+	    quit()
+    if operation_from_user == '8':
+        remotely_shutdown_system()
 
 
 process_operation_gotten_from_user(operation)
